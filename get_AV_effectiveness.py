fabe1ebb17ac7e65af3c5f748a1bab1946d99326
Emil Göransson <emilgo@kth.se>
2025-06-10 16:16:50 +0200

    format

--- get_AV_effectiveness.py
+++ get_AV_effectiveness.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
         for av_name in first_upload_results:
             anti_malware_dict[av_name] = {"hits": 0, "scans": 0}
 
-    for upload in db.get("uploads", []):
-        results = upload.get("data", {}).get("data", {}).get("attributes", {}).get("results", {})
+    for upload in db["uploads"]:
+        results = upload["data"]["data"]["attributes"]["results"]
 
         for name, result_data in results.items():
             if name in anti_malware_dict:
-                if not result_data.get("category") in ["type-unsupported"]:
+                if not result_data["category"] in ["type-unsupported"]:
                     anti_malware_dict[name]["scans"] += 1
                     # If its detected
-                    if result_data.get("category") in ["malicious", "suspicious"]:
+                    if result_data["category"] in ["malicious", "suspicious"]:
                         anti_malware_dict[name]["hits"] += 1
     sorted = dict(sorted(anti_malware_dict.items(), key=lambda item: item[1]['hits'], reverse=True))
     print(sorted)
